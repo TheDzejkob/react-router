b0c87d9c97a2cce3a05f7dc246300f7f1ce04323
TheDzejkob <125360256+TheDzejkob@users.noreply.github.com>
2024-04-23 08:09:35 +0000

    zaklad fork z rajilic routeru

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Eshop from "./pages/Eshop";
+import ContactUs from "./pages/ContanctUs"
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<Home />}>
             <Route path="home" element={<Home />} />
+            <Route path="eshop" element={<Eshop/>}/>
+            <Route path="contactus" element={<ContactUs/>}/>
             {/* <Route path="*" element={<NoPage />} /> */}
           </Route>
         </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <Link class="nav-link active" to="/home">
+          Home
+        </Link>
+      </li>
+      <li class="nav-item">
+      <Link class="nav-link active" to="/eshop">
+          Eshop
+        </Link>
+      </li>
+      <li class="nav-item">
+      <Link class="nav-link active" to="/contactus">
+          ContactUs
+        </Link>
+      </li>
+
+    </ul>
+  </div>
+</nav>
   )
 }
 
